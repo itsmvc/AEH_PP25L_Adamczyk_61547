addf82b2812996195fa3e3ebeeea541d20ec9de2
itsmvc <96144812+itsmvc@users.noreply.github.com>
2025-03-13 01:17:52 +0100

    Pierwszy zapis do repozytorium Lab2

---
+++ src/pl/pp/mojaDrugaAplikacja.java
@@ -0,0 +1,12 @@
+package pl.pp;
+import java.util.Scanner;
+public class mojaDrugaAplikacja {
+    public static void main(String[] args) {
+        double x = 10;
+        var result = x * 2;
+        System.out.println("x = " + x);
+        System.out.println("x * 2 = " + result);
+        result = x*x;
+        System.out.println("x^2 = " + result);
+    }
+}
